ecb0e40d86dae852b6d0e503152fee39b7a9555f
Vagios Vlachos <bagios234@gmail.com>
2015-12-15 15:09:58 +0200

    Lesson 7

--- leaderboard.js
+++ leaderboard.js
@@ -2,7 +2,25 @@ PlayerList = new Mongo.Collection('players');
 
 
 if (Meteor.isClient) {
+  Template.leaderboard.helpers({
+    'player': function() {
+      return PlayerList.find();
+    },
+    'selectedClass': function() {
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if (selectedPlayer == playerId) {
+        return 'selected';
+      };
+    }
+  });
 
+  Template.leaderboard.events({
+    'click .player': function() {
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    }
+  });
 }
 
 if (Meteor.isServer) {
